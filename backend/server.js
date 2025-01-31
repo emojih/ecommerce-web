1e15ae12c1b620fd19d2b92a6ce39e6a433396ec
emojih <emmanuelojih112@gmail.com>
2025-01-31 16:24:56 +0100

    backend apis for login and products created

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
-
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 
 // App Config
 
@@ -18,6 +19,9 @@ app.use(express.json())
 app.use(cors())
 
 // api endpoints
+
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
 app.get('/', (req, res) => {
   res.send('API Working')  
