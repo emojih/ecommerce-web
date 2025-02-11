1af38e4f4aaa67d9f5d06e83a2b1dbd198890146
emojih <emmanuelojih112@gmail.com>
2025-02-11 18:01:46 +0100

    order placed api mapped

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,35 @@
-
-
+import orderModel from '../models/orderModel.js'
+import userModel from '../models/userModel.js'
 // Placing orders using COD Method
 
 const placeOrder = async (req,res) => {
     
+    try {
+        const {userId, items, amount, address} = req.body;
+
+        const orderData = {
+            userId,
+            items, 
+            address,
+            amount, 
+            paymentMethod: 'COD',
+            payment:false,
+            date:Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId,{cartData:{}})
+
+        res.json({success:true,message:'Order Placed'})
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+        
+    }
 }
 // Placing orders using stripe Method
 
@@ -18,4 +44,19 @@ const placeOrderRazorpay = async (req,res) => {
 
 // All Orders data for admin panel
 
+const allOrders = async (req,res) => {
 
+}
+
+// User Order data for Frontend
+const userOrders = async (req,res) => {
+
+}
+
+// update order status from admin panel
+
+const updateStatus = async (req,res) => {
+
+}
+
+export {placeOrder,placeOrderStripe,placeOrderRazorpay,allOrders,userOrders,updateStatus}
