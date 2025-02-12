7deaf6d6541a5dc29818b918796c50fca9548f01
emojih <emmanuelojih112@gmail.com>
2025-02-12 16:21:07 +0100

    orders showing on the admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -46,10 +46,29 @@ const placeOrderRazorpay = async (req,res) => {
 
 const allOrders = async (req,res) => {
 
+    try {
+        const orders = await orderModel.find({})
+res.json({success:true,orders})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+
+
 }
 
 // User Order data for Frontend
 const userOrders = async (req,res) => {
+
+    try {
+        const {userId} = req.body
+        const orders = await orderModel.find({userId})
+        res.json({success:true,orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 
 }
 
