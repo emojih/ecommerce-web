6f6c07039fb0e4fc2a9bb749e54bf9de0c4e3a8f
emojih <emmanuelojih112@gmail.com>
2026-02-02 14:10:28 +0100

    fixes on search

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
+import { useLocation } from "react-router-dom";
+
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
@@ -13,6 +15,7 @@ const Navbar = () => {
     setToken,
     setCartItems,
   } = useContext(ShopContext);
+  const location = useLocation();
 
   const logout = () => {
     localStorage.removeItem("token");
@@ -53,8 +56,9 @@ const Navbar = () => {
       </ul>
       <div className="flex items-center gap-6">
         {/* Show search icon only on both page */}
-        {(location.pathname.includes("/collection") ||
-          location.pathname.includes("/brands")) && (
+        {(location.pathname === "/" ||
+          location.pathname.includes("collection") ||
+          location.pathname.includes("brands")) && (
           <img
             onClick={() => setShowSearch(true)}
             src={assets.search_icon}
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -2,17 +2,21 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 const SearchBar = () => {
   const { search, setSearch, showSearch, setShowSearch } =
     useContext(ShopContext);
 
   const location = useLocation();
   const [visible, setVisible] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (
       location.pathname.includes("collection") ||
-      location.pathname.includes("brands")
+      location.pathname.includes("brands") ||
+      location.pathname.includes("/")
     ) {
       setVisible(true);
     } else {
@@ -24,13 +28,20 @@ const SearchBar = () => {
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input
           value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) => {
+            setSearch(e.target.value);
+
+            if (!location.pathname.includes("collection")) {
+              navigate("/collection");
+            }
+          }}
           type="text"
           placeholder="Search"
           className="flex-1 outline-none bg-inherit text-sm"
         />
         <img src={assets.search_icon} className="w-4" alt="" />
       </div>
+
       <img
         onClick={() => setShowSearch(false)}
         src={assets.cross_icon}
